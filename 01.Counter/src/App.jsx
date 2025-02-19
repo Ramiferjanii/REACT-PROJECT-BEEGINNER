cf59d939a70c50bea124accade04025a26f6496a
Rami Ben Ferjani <ramiferjani.20@gmail.com>
2025-02-19 17:39:05 +0000

    Add files via upload

---
+++ 01.Counter/src/App.jsx
@@ -0,0 +1,19 @@
+import Counter from "./components/Counter"; // import the Counter component
+
+
+
+
+
+const App = () => {
+  return (
+
+
+    <div>
+      <Counter /> {/* render the Counter component */}
+    </div>
+
+
+  )
+}
+
+export default App;
